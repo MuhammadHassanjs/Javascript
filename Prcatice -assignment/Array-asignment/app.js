f33ba43b03361fed582a850d8190d1fb1cc02e13
MuhammadHassanjs <156912014+MuhammadHassanjs@users.noreply.github.com>
2024-03-04 04:11:39 +0500

    Half Done

---
+++ Prcatice -assignment/Array-asignment/app.js
@@ -0,0 +1,53 @@
+// 1. Declare an empty array using JS literal notation to store
+// student names in future.
+
+   // let emptyarray
+   // emptyarray=["Hassan","ashraf",20]
+   // console.log(emptyarray)
+
+//    2. Declare an empty array using JS object notation to store
+//    student names in future.
+
+   // let emptyarray
+   // emptyarray=["Hassan","ashraf",20]
+   // console.log(emptyarray)
+
+//    3. Declare and initialize a strings array.
+         
+   // let emptyarray
+   // emptyarray=["Hassan","ashraf",20]
+   // console.log(emptyarray)
+
+//    4. Declare and initialize a numbers array.
+     
+     // let numbers=[10,20,30,40,50]
+     // console.log(numbers);
+//  5. Declare and initialize a boolean array.
+
+    // let bolArray=[ 5>6 , 9>8 , 6>2]
+    // console.log(bolArray);
+
+// 6. Declare and initialize a mixed array.
+
+    // let mixArray=["Hassan","Ashraf",20,true]
+    // console.log(mixArray);
+
+// 7. Declare and Initialize an array and store available mobile
+//     networks in Pakistan.
+
+// let MobNetwork=["Jazz" ,"Moblink ","Warid" ,"Telenor"]
+// console.log(updatedMobnetwork);
+
+
+// 8. Declare and Initialize an array and store available education
+// qualifications in Pakistan (e.g. SSC, HSC, BCS, BS, BCOM, MS,
+// M. Phil., PhD). Show the listed qualifications in your browser
+// like:
+
+    // let education=["SSC", "HSC", "BCS", "BS", "BCOM", "MS", "M. Phil"," PhD"]
+    // document.write(`1) ${education[0]} <br/ > 2) ${education[1]} <br/ > 3) ${education[2]} <br/ > 4) ${education[3]} <br/ > 5) ${education[4]} <br/ > 6) ${education[5]} <br/ > 7) ${education[6]}  `)
+
+// 9. Declare and initialize an empty array to store top movies of
+// 2015. Add movies one by one in an array. Display the elements
+// & length of array in your browser. (Use array’s length method)
+
